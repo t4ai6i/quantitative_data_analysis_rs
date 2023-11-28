7aa2a43d3f3d966c09f8892a0e6ab4d0ccb8e2fd
t4ai6i <t4ai6i>
2023-11-28 21:02:29 +0900

    Change visibility

--- src/domain/entity/company.rs
+++ src/domain/entity/company.rs
@@ -2,7 +2,7 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug, Clone, Eq, PartialEq, Ord, PartialOrd, Default)]
 pub struct Company {
-    pub(crate) code: String,
-    pub(crate) name: String,
-    pub(crate) market: String,
+    pub code: String,
+    pub name: String,
+    pub market: String,
 }
