44f5f5626758725cc0f775dd831bfc62c715e084
t4ai6i <t4ai6i>
2024-08-07 17:02:27 +0900

    Specify has_headers arg by const generic

--- src/infrastructure/from_slice.rs
+++ src/infrastructure/from_slice.rs
@@ -15,26 +15,11 @@ pub trait FromSlice {
 
     fn from_slice<const B: bool>(value: &[u8]) -> Vec<Self::Item> {
         let mut reader = match Self::data_format() {
-            DataFormat::CSV => {
-                if B {
-                    ReaderBuilder::new().has_headers(true).from_reader(value)
-                } else {
-                    ReaderBuilder::new().has_headers(false).from_reader(value)
-                }
-            }
-            DataFormat::TSV => {
-                if B {
-                    ReaderBuilder::new()
-                        .delimiter(b'\t')
-                        .has_headers(true)
-                        .from_reader(value)
-                } else {
-                    ReaderBuilder::new()
-                        .delimiter(b'\t')
-                        .has_headers(false)
-                        .from_reader(value)
-                }
-            }
+            DataFormat::CSV => ReaderBuilder::new().has_headers(B).from_reader(value),
+            DataFormat::TSV => ReaderBuilder::new()
+                .delimiter(b'\t')
+                .has_headers(B)
+                .from_reader(value),
         };
         reader
             .deserialize::<Self::Deserialize>()
