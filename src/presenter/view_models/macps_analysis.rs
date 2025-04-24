c8ee344860e0619f3326b66d5a9aaf50a8d9ee6f
t4ai6i <t4ai6i>
2025-04-24 21:27:03 +0900

    refactor: move MACPSAnalysis struct to view_model module

--- src/presenter/view_models/macps_analysis.rs
+++ src/presenter/view_models/macps_analysis.rs
@@ -1,11 +1,1 @@
-use crate::domain::models::buy_sell_signal::model::BuySellSignalType;
-use crate::shared::custom_date_format;
-use chrono::NaiveDate;
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug, Copy, Clone, PartialEq, PartialOrd, Default)]
-pub struct MACPSAnalysis {
-    pub r#type: BuySellSignalType,
-    #[serde(with = "custom_date_format")]
-    pub date: NaiveDate,
-}
+pub mod view_model;
